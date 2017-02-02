f0d1212b8a6f2f7d7c9ff756515a061ee1d03a5f
SleJP <j.p.g.sleddens@hr.nl>
2017-02-02 12:24:50 +0100

    Now handles multiple -d/--dir parameters to scan multiple locations within one call of the script.

--- wordpress_version_check.php
+++ wordpress_version_check.php
@@ -12,26 +12,35 @@ function print_usage() {
 }
 
 $options = getopt("hvd:", array("help", "verbose", "dir:"));
-if (isset($options["d"]) && is_dir($options["d"])) {
-  define("SCANDIR", $options["d"]);
-} elseif (isset($options["dir"]) && is_dir($options["dir"])) {
-  define("SCANDIR", $options["dir"]);
-}
 
 if (isset($options["h"]) || isset($options["help"])) {
   print_usage();
   exit(0);
 }
 
-if (!defined("SCANDIR")) {
-  print_usage();
-  exit(1);
-}
-
 if (isset($options["v"]) || isset($options["verbose"])) {
   define("VERBOSE", true);
 } else {
   define("VERBOSE", false);
+}
+
+$scandirs = array();
+foreach (array("d", "dir") as $optionname) {
+  if (isset($options[$optionname])) {
+    if (is_array($options[$optionname])) {
+      foreach ($options[$optionname] as $dir) {
+        if (is_dir($dir)) $scandirs[] = $dir;
+      }
+    } elseif (is_dir($options[$optionname])) {
+      $scandirs[] = $options[$optionname];
+    }
+  }
+}
+define("SCANDIR", $scandirs);
+
+if (count(SCANDIR) == 0) {
+  print_usage();
+  exit(1);
 }
 
 $users = array();
@@ -84,66 +93,68 @@ if (($version_hash_current != $version_hash_cache) && // New versions have been 
 $wp_current_versions = array_unique($wp_current_versions);
 
 if (filemtime(VERSION_CACHE_FILE) > (time() - (24 * 60 * 60))) {
-  mail("cmi-beheer@hr.nl", "Wordpress Version Check - Versions Updated", "The latest version file has been updated, you might want to check it:\n" . var_dump($wp_current_versions));
+  mail("cmi-beheer@hr.nl", "Wordpress Version Check - Versions Updated", "The latest version file has been updated, you might want to check it:\n" . print_r($wp_current_versions, true));
   exit(0); // Give it one more day
 }
 
 if (VERBOSE) print("Outdated Wordpress sites:\n");
 if (VERBOSE) print("------------------------------------------------------------------------------\n");
 
-$di = new RecursiveDirectoryIterator(SCANDIR, FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::SKIP_DOTS);
-foreach (new RecursiveIteratorIterator($di, RecursiveIteratorIterator::SELF_FIRST) as $direntry) {
-  if ($direntry->getFileName() == "version.php") {
-    if (preg_match("/wp-includes\/?/", $direntry->getPath())) {
-      $wp_version = "";
-      $wp_basedir = $direntry->getPathname();
-      $fh = @fopen($wp_basedir, "r");
-      if ($fh) {
-        while (($buffer = fgets($fh, 4096)) !== false) {
-          $matches = array();
-          if (preg_match('/^\s*\$wp_version\s*=\s*["\'](.*?)["\']\s*;/', $buffer, $matches)) {
-            $wp_version = $matches[1];
-            $wp_basedir = preg_replace("/wp-includes\/version.php$/", "", $wp_basedir);
-            $htaccess_file = $wp_basedir . "/blocked_please_update_wordpress";
-            if (in_array($wp_version, $wp_current_versions) === false) {
-              // Old version detected
-              if (VERBOSE) print("  Basedir: $wp_basedir\n");
-              if (VERBOSE) print("  Version: $wp_version\n");
-              $wp_owner = get_user_by_path($wp_basedir);
-              if (VERBOSE) print("  Owner:   $wp_owner\n");
-              if (!is_file($htaccess_file) || md5_file($htaccess_file) != $htaccess_contents_md5) {
-                $mail_to = "";
-                if (substr($wp_owner, 0, 4) == "prj_" || substr($wp_owner, 0, 5) == "vgrp_") {
-                  $group = posix_getgrnam($wp_owner);
-                  foreach ($group["members"] as $member) $mail_to .= ", " . $member . "@hr.nl";
-                } else $mail_to = $wp_owner . "@hr.nl";
-                $mail_to = preg_replace("/^, /", "", $mail_to);
-                file_put_contents($htaccess_file, $htaccess_contents);
-                if ($mail_to != "") {
-                  $headers = "From: \"CMI Serverbeheer\" <cmi-beheer@hr.nl>\r\n";
-                  $headers.= "Cc: \"CMI Serverbeheer\" <cmi-beheer@hr.nl>\r\n";
-                  $message = "Wij hebben een verouderde versie van Wordpress op je CMI webspace gevonden. Deze oude versies zijn erg onveilig voor het netwerk en kunnen misbruikt worden door hackers; dit kan grote consequenties hebben voor de Hogeschool Rotterdam.\r\n\r\nBij deze wordt je dan ook verzocht de Wordpress site te updaten naar de laatste versie. De nieuwste versies van Wordpress bieden ook een \"auto-update\" optie, waardoor je hierna altijd de laatste en veilige versie van Wordpress zal draaien.\r\n\r\nDe Wordpress site is vanaf nu van buiten het netwerk van Hogeschool Rotterdam niet meer te bereiken doordat we een bestand genaamd \"blocked_please_update_wordpress\" in je webspace hebben gezet. Als je de Wordpress site wilt updaten moet je eerst dat bestand via SFTP of SSH toegang verwijderen en daarna kun je Wordpress normaal updaten.\r\n\r\n\r\n\r\nWe have found an old version of Wordpress in your CMI webspace. These old versions are very unsecure and pose a threat to the network of Rotterdam University as they can be abused by hackers; this can have major consequences for Rotterdam University.\r\n\r\nWe urge you to update your Wordpress site as soon as possible to the latest version. The latest versions of Wordpress offer an \"auto-update\" option, this option makes sure that you will always run the latest and secure version of Wordpress.\r\n\r\Your outdated Wordpress site is now blocked from outsite the University network because we have placed a \"blocked_please_update_wordpress\" file in your webspace. If you want to update your Wordpress site you first have to delete that file through SFTP or SSH access and after that you can update Wordpress normally.\r\n\r\n\r\n\r\nUsername: $wp_owner\r\nWordpress directory: $wp_basedir\r\nInstalled version: $wp_version\r\n\r\nThe latest available version of Wordpress can be downloaded from http://wordpress.org";
-                  mail($mail_to, "Oude Wordpress site / Old Wordpress site", $message, $headers);
-                  if (VERBOSE) print("  Action:  blocked and mail sent to $mail_to\n");
+foreach (SCANDIR as $scandir) {
+  $di = new RecursiveDirectoryIterator($scandir, FilesystemIterator::CURRENT_AS_FILEINFO | FilesystemIterator::SKIP_DOTS);
+  foreach (new RecursiveIteratorIterator($di, RecursiveIteratorIterator::SELF_FIRST) as $direntry) {
+    if ($direntry->getFileName() == "version.php") {
+      if (preg_match("/wp-includes\/?/", $direntry->getPath())) {
+        $wp_version = "";
+        $wp_basedir = $direntry->getPathname();
+        $fh = @fopen($wp_basedir, "r");
+        if ($fh) {
+          while (($buffer = fgets($fh, 4096)) !== false) {
+            $matches = array();
+            if (preg_match('/^\s*\$wp_version\s*=\s*["\'](.*?)["\']\s*;/', $buffer, $matches)) {
+              $wp_version = $matches[1];
+              $wp_basedir = preg_replace("/wp-includes\/version.php$/", "", $wp_basedir);
+              $htaccess_file = $wp_basedir . "/blocked_please_update_wordpress";
+              if (in_array($wp_version, $wp_current_versions) === false) {
+                // Old version detected
+                if (VERBOSE) print("  Basedir: $wp_basedir\n");
+                if (VERBOSE) print("  Version: $wp_version\n");
+                $wp_owner = get_user_by_path($wp_basedir);
+                if (VERBOSE) print("  Owner:   $wp_owner\n");
+                if (!is_file($htaccess_file) || md5_file($htaccess_file) != $htaccess_contents_md5) {
+                  $mail_to = "";
+                  if (substr($wp_owner, 0, 4) == "prj_" || substr($wp_owner, 0, 5) == "vgrp_") {
+                    $group = posix_getgrnam($wp_owner);
+                    foreach ($group["members"] as $member) $mail_to .= ", " . $member . "@hr.nl";
+                  } else $mail_to = $wp_owner . "@hr.nl";
+                  $mail_to = preg_replace("/^, /", "", $mail_to);
+                  file_put_contents($htaccess_file, $htaccess_contents);
+                  if ($mail_to != "") {
+                    $headers = "From: \"CMI Serverbeheer\" <cmi-beheer@hr.nl>\r\n";
+                    $headers.= "Cc: \"CMI Serverbeheer\" <cmi-beheer@hr.nl>\r\n";
+                    $message = "Wij hebben een verouderde versie van Wordpress op je CMI webspace gevonden. Deze oude versies zijn erg onveilig voor het netwerk en kunnen misbruikt worden door hackers; dit kan grote consequenties hebben voor de Hogeschool Rotterdam.\r\n\r\nBij deze wordt je dan ook verzocht de Wordpress site te updaten naar de laatste versie. De nieuwste versies van Wordpress bieden ook een \"auto-update\" optie, waardoor je hierna altijd de laatste en veilige versie van Wordpress zal draaien.\r\n\r\nDe Wordpress site is vanaf nu van buiten het netwerk van Hogeschool Rotterdam niet meer te bereiken doordat we een bestand genaamd \"blocked_please_update_wordpress\" in je webspace hebben gezet. Als je de Wordpress site wilt updaten moet je eerst dat bestand via SFTP of SSH toegang verwijderen en daarna kun je Wordpress normaal updaten.\r\n\r\n\r\n\r\nWe have found an old version of Wordpress in your CMI webspace. These old versions are very unsecure and pose a threat to the network of Rotterdam University as they can be abused by hackers; this can have major consequences for Rotterdam University.\r\n\r\nWe urge you to update your Wordpress site as soon as possible to the latest version. The latest versions of Wordpress offer an \"auto-update\" option, this option makes sure that you will always run the latest and secure version of Wordpress.\r\n\r\Your outdated Wordpress site is now blocked from outsite the University network because we have placed a \"blocked_please_update_wordpress\" file in your webspace. If you want to update your Wordpress site you first have to delete that file through SFTP or SSH access and after that you can update Wordpress normally.\r\n\r\n\r\n\r\nUsername: $wp_owner\r\nWordpress directory: $wp_basedir\r\nInstalled version: $wp_version\r\n\r\nThe latest available version of Wordpress can be downloaded from http://wordpress.org";
+                    mail($mail_to, "Oude Wordpress site / Old Wordpress site", $message, $headers);
+                    if (VERBOSE) print("  Action:  blocked and mail sent to $mail_to\n");
+                  } else {
+                    if (VERBOSE) print("  Action:  blocked, but no mail sent\n");
+                  }
                 } else {
-                  if (VERBOSE) print("  Action:  blocked, but no mail sent\n");
+                  if (VERBOSE) print("  Action:  none, was already blocked\n");
                 }
-              } else {
-                if (VERBOSE) print("  Action:  none, was already blocked\n");
-              }
-              if (VERBOSE) print("------------------------------------------------------------------------------\n");
-            } else {
-              // Acceptable version detected
-              if (is_file($htaccess_file)) {
-                // Unblock the site, apparantly it has been updated
-                unlink($htaccess_file);
-                if (VERBOSE) print("Unblocked: $wp_basedir ($wp_version)\n");
                 if (VERBOSE) print("------------------------------------------------------------------------------\n");
+              } else {
+                // Acceptable version detected
+                if (is_file($htaccess_file)) {
+                  // Unblock the site, apparantly it has been updated
+                  unlink($htaccess_file);
+                  if (VERBOSE) print("Unblocked: $wp_basedir ($wp_version)\n");
+                  if (VERBOSE) print("------------------------------------------------------------------------------\n");
+                }
               }
             }
           }
+          fclose($fh);
         }
-        fclose($fh);
       }
     }
   }
